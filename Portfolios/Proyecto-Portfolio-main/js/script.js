d6d4e644bc4aaf260b186f664779e5ce18604713
Gabyboom <gabrielburgos1778@gmail.com>
2024-08-21 11:01:26 +0800

    first commit

---
+++ Portfolios/Proyecto-Portfolio-main/js/script.js
@@ -0,0 +1,7 @@
+/*_____________Typing animation_______________________________*/
+var typed = new Typed(".typing", {
+    strings: ["Desarrollador Web", "Diseñador Web","Diseñador Gráfico","Youtuber"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+})
